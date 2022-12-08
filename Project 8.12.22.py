fa88e5d99fc0d4c05dc1d41c0693af22a4b9a93a
Robert <92167234+17RPakulski@users.noreply.github.com>
2022-12-08 13:16:32 +0000

    Add files via upload

---
+++ Project 8.12.22.py
@@ -0,0 +1,7 @@
+# Greet user
+print('Hello, Welcome to BlackJack')
+
+# single player or Multiplayer
+s_m = int(input('Would you like to play Single-Player or Multi-Player?\n\n1: Single-player     2: Multi-Player\nSingle-Player or Multl-Player: '))
+
+if
