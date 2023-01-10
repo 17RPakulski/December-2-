a397fcf175c8ca22dcafa7a907253d9b95249e68
Robert <92167234+17RPakulski@users.noreply.github.com>
2023-01-10 10:02:11 +0000

    Add files via upload

---
+++ if single of multi code 10.1.23.py
@@ -0,0 +1,11 @@
+# single player or Multiplayer
+'''ADD LATER:
+s_m = int(input('Would you like to play Single-Player or Multi-Player?\n\n1: Single-player     2: Multi-Player\nEnter Single-Player or Multi-Player: '))
+if s_m == 1:
+    print('-------------------------')
+    p1 = input('Player 1 Name: ')
+else:
+    print('-------------------------')
+    p1 = input('Player 1 Name: ')
+    p2 = input('Player 2 Name: ')
+'''
