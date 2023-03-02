ad54c3893e38bec73b966f3dbb3260f08fd7a93e
Robert <92167234+17RPakulski@users.noreply.github.com>
2023-03-02 13:16:08 +0000

    Add files via upload

---
+++ notes teacher.py
@@ -0,0 +1,20 @@
+create a list of players - a list of lists or a dictionary of dictionaries
+
+give card(): should take the number of cards and the player id
+https://stackoverflow.com/questions/6588056/python-can-a-dict-have-a-value-that-is-a-list
+d = {}
+d["Player1"] = []
+d.
+
+Distribute the cards according to the number of cards and players - all cards have to be dealt in a circle so don't do dealer first and then players
+
+checkwin() - 1) if one player has 21 and others don't that player wins
+                else if more than one player has 21 its a tie m oney is returned
+                else if some players have more than 21 they are bust
+                
+stand() - no change to hands
+
+hit() - an extra card is dealt so call give card with number of cards and player name
+
+
+
